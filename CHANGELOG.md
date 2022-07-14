50062d366802508c13ba6a99b7eefee3656b2f7a
AssisrMatheus <assisr.matheus@gmail.com>
2022-07-14 10:37:32 -0400

    feat: adds `stripNullFromObject` and `stripUndefinedAndNullFromObject`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+## [0.4.0] - 2022-07-14
+
+### Added
+
+- Adds `stripNullFromObject` and `stripUndefinedAndNullFromObject`
+
 ## [0.3.2] - 2022-06-20
 
 ### Fixed
--- src/helpers/object/index.ts
+++ src/helpers/object/index.ts
@@ -1,8 +1,36 @@
 /**
- * Strips out from object keys that have undefined as value
+ * Strips out from object keys that have `undefined` as value
  *
  * @param obj The object you want to remove undefined keys from
  * @returns a new instance of the object, with undefined values removed
  */
 export const stripUndefinedFromObject = <T extends Record<string, unknown>>(obj: T) =>
   JSON.parse(JSON.stringify(obj)) as T;
+
+/**
+ * Strips out from object keys that have `null` as value
+ *
+ * @param obj The object you want to remove null keys from
+ * @returns a new instance of the object, with null values removed
+ */
+export const stripNullFromObject = <T extends Record<string, unknown>>(obj: T): T =>
+  Object.fromEntries(
+    Object.entries(obj)
+      .filter(([, value]) => value !== null)
+      .map(
+        ([key, value]) =>
+          [key, value === Object(value) ? stripNullFromObject(value as Record<string, unknown>) : value] as [
+            string,
+            unknown
+          ]
+      )
+  ) as T;
+
+/**
+ * Strips out from object keys that have `undefined` or `null` as value
+ *
+ * @param obj The object you want to remove undefined and null keys from
+ * @returns a new instance of the object, with undefined and null values removed
+ */
+export const stripUndefinedAndNullFromObject = <T extends Record<string, unknown>>(obj: T) =>
+  stripNullFromObject(stripUndefinedFromObject(obj)) as T;
